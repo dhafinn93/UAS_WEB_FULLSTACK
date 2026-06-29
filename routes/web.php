3686a4ffaaf6e641b04a0e95af6b9719b32cb262
dhafinn93 <minnoru55@gmail.com>
2026-06-29 11:02:47 +0700

    membuat route buat website

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\KosController;
+use App\Http\Controllers\User\ReviewController;
+use App\Http\Controllers\User\DashboardController as UserDashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Admin
+Route::middleware(['auth','admin'])->group(function(){
+
+    Route::get('/admin/dashboard',[DashboardController::class,'index']);
+    Route::resource('admin/kos',KosController::class);
 });
+
+//User
+Route::middleware(['auth','user'])->group(function(){
+
+    Route::get('/user/dashboard', [UserDashboardController::class,'index']);
+    Route::post('/user/review', [ReviewController::class,'store']);
+    Route::get('/user/reviews', [ReviewController::class,'index']);
+});
+
+//publik
+Route::get('/',[HomeController::class,'index']);
+Route::get('/kos/{id}',[HomeController::class,'show']);
+Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+Route::post('/login',[AuthController::class,'login']);
+Route::get('/register',[AuthController::class,'showRegister']);
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
