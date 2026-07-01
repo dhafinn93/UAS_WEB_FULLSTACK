9cbe32719f8e3bcaa56fca0c84112eb48fbc9172
Regan piko <reganpiko@gmail.com>
2026-07-01 16:45:04 +0700

    menghapus home dan regis

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,69 @@
 @section('title', 'Home')
 
 @section('content')
-<h1>Selamat Datang</h1>
+
+<section class="hero">
+    <div class="container text-center">
+        <h1>Cari Kos Impianmu</h1>
+        <p class="mt-3">Temukan kos nyaman dengan harga terbaik.</p>
+
+        <form class="row justify-content-center mt-4">
+            <div class="col-md-5">
+                <input type="text" class="form-control form-control-lg" placeholder="Cari nama kos...">
+            </div>
+            <div class="col-md-2 mt-3 mt-md-0"> 
+                <button type="submit" class="btn btn-warning btn-lg w-100">
+                    <i class="fa fa-search"></i> Cari
+                </button>
+            </div>
+        </form>
+    </div>
+</section>
+
+<div class="container mt-5">
+    <h3 class="mb-4">Kos Tersedia</h3>
+
+    <div class="row">
+        @if(isset($kos))
+            @forelse($kos as $item)
+                <div class="col-md-4 mb-4">
+                    <div class="card card-kos h-100"> 
+                        
+                        @if($item->foto)
+                            <img src="{{ asset('storage/' . $item->foto) }}" class="card-img-top" style="height: 230px; object-fit: cover;" alt="Foto {{ $item->nama }}">
+                        @else
+                            <img src="https://via.placeholder.com/400x230" class="card-img-top" alt="Placeholder Foto">
+                        @endif
+
+                        <div class="card-body d-flex flex-column"> 
+                            <h5 class="card-title">{{ $item->nama }}</h5>
+                            <p class="card-text text-muted">
+                                <i class="fa fa-location-dot text-danger"></i> {{ $item->alamat }}
+                            </p>
+                            
+                            <p class="harga mt-auto"> 
+                                Rp {{ number_format($item->harga, 0, ',', '.') }}
+                            </p>
+                            <a href="/kos/{{ $item->id }}" class="btn btn-primary w-100">Lihat Detail</a>
+                        </div>
+                        
+                    </div>
+                </div>
+            @empty
+                <div class="col-12">
+                    <div class="alert alert-warning text-center">
+                        Belum ada data kos.
+                    </div>
+                </div>
+            @endforelse
+        @else
+            <div class="col-12">
+                <div class="alert alert-info text-center">
+                    Controller belum mengirim data <b>$kos</b>.
+                </div>
+            </div>
+        @endif
+    </div>
+</div>
+
 @endsection
